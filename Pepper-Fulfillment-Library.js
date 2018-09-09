ab4196936ff5fc7f88051401a404c820db79bb42
DanieltheYogi <daniel.lane.schofield@gmail.com>
2018-09-09 13:20:50 -0700

    Update Pepper-Fulfillment-Library.js

--- Pepper-Fulfillment-Library.js
+++ Pepper-Fulfillment-Library.js
@@ -630,5 +630,31 @@ function randomlyChoose(array){
     return array[Math.floor(Math.random() * array.length)];
 }
 
+function sendResponseToDialogflow (response, responseToUser) {
+    //console.log(JSON.stringify(responseToUser);
+    // if the response is a string send it as a response to the user
+    if (typeof responseToUser === 'string') {
+      let responseJson = {};
+      responseJson.speech = responseToUser; // spoken response
+      responseJson.displayText = responseToUser; // displayed response
+      response.json(responseJson); // Send response to Dialogflow
+    } else {
+      // If the response to the user includes rich responses or contexts send them to Dialogflow
+      let responseJson = {};
+      // If speech or displayText is defined, use it to respond (if one isn't defined use the other's value)
+      responseJson.speech = responseToUser.speech || responseToUser.displayText || "";
+      responseJson.displayText = responseToUser.displayText || responseToUser.speech;
+      responseJson.messages = responseToUser.messages;
+      // Optional: add contexts (https://dialogflow.com/docs/contexts)
+      if (responseToUser.contextOut)
+        responseJson.contextOut = responseToUser.contextOut;
+      if (responseToUser.followupEvent)
+        responseJson.followupEvent = responseToUser.followupEvent;
+      responseJson.data = responseToUser.data;
+      console.log("RESPONSE TO DIALOGFLOW COMPLETE: ", JSON.stringify(responseJson);
+      response.json(responseJson); // Send response to Dialogflow
+    }
+}
+
 module.exports = { BackgroundImage, BasicCard, BasicText, CarouselImage, Carousel, CarouselImageNoTitle, CarouselNoTitles, FullScreenImage, 
-    Icon, Icons, Style, TextBubble, TextBubbles, TriggerIntent, Video, Website, PepperResponse, toTitleCase, randomlyChoose };
+    Icon, Icons, Style, TextBubble, TextBubbles, TriggerIntent, Video, Website, PepperResponse, toTitleCase, randomlyChoose, sendResponseToDialogflow};
